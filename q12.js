35b279eda0e70b04d6183a71b0cc07ee62c9e0f2
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-20 22:29:59 +0200

    skip 12 qs for yet **

---
+++ q12.js
@@ -0,0 +1,12 @@
+function run() {
+     const input = document.getElementById("input").value;
+     const output = document.getElementById("output");
+     let out = "";
+ /**/
+     // write your code here
+
+}
+     // out =;
+ 
+ /**/
+     output.innerText = out;
