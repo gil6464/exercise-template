c7ea50bb74451ae67b0d7d510cfff1c0c35bfd9e
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-20 19:48:32 +0200

    qs 9  solve

---
+++ q9.js
@@ -0,0 +1,19 @@
+function run() {
+     const input = document.getElementById("input").value;
+     const output = document.getElementById("output");
+     let out = "";
+ /**/
+     // write your code here
+          const originArr = JSON.parse(input);
+          out = [];
+          originArr.sort();
+          for (i = 0; i < originArr.length -1 ; i++){
+               if (originArr[i] === originArr[i+1] &&
+                     originArr[i] !==originArr[i+2]){
+                    out.push(originArr[i])
+                     }
+                    }
+ 
+ /**/
+     output.innerText = "Duplicate values : "+out ;
+ }
