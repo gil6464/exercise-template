f335052f41f083ae122662258e7df68075aca754
gil6464 <77244365+gil6464@users.noreply.github.com>
2021-01-24 23:18:22 +0200

    qs 12 solved
    i tryed to solved this qwstion with map(); method but probably i do somthing wrong and its not go well.. also try to use split( ', '); on the input to make it array but also with no sucsess, so i used the eval();

--- q12.js
+++ q12.js
@@ -1,13 +1,20 @@
 function run() {
      const input = document.getElementById("input").value;
+     const input2 = document.getElementById("input2").value;
      const output = document.getElementById("output");
-     let out = "";
+     let out = '';
  /**/
      // write your code here
+     const array = eval(input2);
+     let list = [];
      
+     for (i = 0; i < array.length; i++){
+         list.push(array[i][input])
+         console.log(list);
+     }
 
-     // out =;
- 
+     out = list;
+
  /**/
      output.innerText = out;
-}
+    }
